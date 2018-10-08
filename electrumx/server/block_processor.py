5985a80cc7d97dab61f20d77d36ceecdb377879b
josh pullen <bolapara@gmail.com>
2018-10-07 23:33:04 -0500

    prevent block processor from swallowing exceptions

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -657,6 +657,9 @@ class BlockProcessor(object):
             async with TaskGroup() as group:
                 await group.spawn(self.prefetcher.main_loop(self.height))
                 await group.spawn(self._process_prefetched_blocks())
+        except Exception as e:
+            self.logger.exception(e)
+            raise e
         finally:
             # Shut down block processing
             self.logger.info('flushing to DB for a clean shutdown...')
